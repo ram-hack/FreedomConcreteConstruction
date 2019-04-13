903d480230c6f579eb9b56fa1953d85540b80473
Michael Gloudeman <mikewr88@yahoo.com>
2019-04-12 18:57:23 -0700

    nav style, footer, and script for trowel animation

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-  
+$(document).ready(function() {
+  $(document).scroll(function(){
+// change if statement math later when more content on Page. account for height of window
+    if($("#header-container").offset().top -100 <= $(document).scrollTop()){
+      $("#trowel-container").css("left","120vw");
+    }
+  })
 });
